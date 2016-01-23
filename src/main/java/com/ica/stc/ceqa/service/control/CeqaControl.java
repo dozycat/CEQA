ce75f4a05f0884f4c1f598e95ce9bc766a5a5a64
dozycat <dozycat@gmail.com>
2016-01-23 15:44:42 +0800

    json支支持

--- src/main/java/com/ica/stc/ceqa/service/control/CeqaControl.java
+++ src/main/java/com/ica/stc/ceqa/service/control/CeqaControl.java
@@ -2,18 +2,20 @@ package com.ica.stc.ceqa.service.control;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /*
  * ceqa的control,调用service层的服务。
  */
 
 @Controller
-@RequestMapping("/")
 public class CeqaControl {
-	
-	
-	@RequestMapping("/hello.io")
+
+	@ResponseBody
+	@RequestMapping(value = "/hello.io", method = RequestMethod.GET)
 	public String test() {
+		System.out.println("hello request!");
 		return ("Hello World");
 	}
 
